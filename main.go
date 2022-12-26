3b22d759e4424e4e94d97b315a5f71ff1f35d49e
OmAxiani0 <aximaniom@gmail.com>
2022-12-26 12:34:46 +0530

    Variables and Constants

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTicket = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTicket, "tickets and", remainingTickets, "are still avainlable")
+	fmt.Println("Get your tickets here to attend")
 }
